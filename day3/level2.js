871a3794d9252702baadca483bf0838c578b53df
shis-ia <charityshisia7139@gmail.com>
2025-01-08 23:04:03 +0300

    boolean operators

--- day3/level2.js
+++ day3/level2.js
@@ -7,7 +7,7 @@ console.log(area); //area of triangel*/
 /*let sideA = prompt('enter sideA');
 let sideB = prompt('enter sideB');
 let sideC = prompt('enter sideC');
-let perimeter = ( sideA + sideB +sideC)
+let perimeter = ( sideA + sideB +sideC);
 console.log(perimeter);*/
 
 /*let length =prompt('enter length');
@@ -16,9 +16,14 @@ let rectarea = ((length * width));
 console.log(rectarea);*/
 
 
-/*let length =prompt('enter length');
+let length =prompt('enter length');
 let width =prompt('width');
-console.log(peimeter= 2 * (length + width));*/
+
+let area = 2 * ( parseInt(length) + parseInt(width) )
+
+console.log(area)
+
+// console.log(2 * (length + width));
 
 /*let radius =prompt('enter radius');
 const PI = (3.14);
@@ -29,7 +34,13 @@ console.log(circlearea);*/
 const PI = (3.14);
 console.log(circumference= (2* PI * radius));*/
 
-let hours= prompt('enter hours');
+/*let hours= prompt('enter hours');
 let rate= prompt('enter rate');
 let pay=(hours*rate);
-console.log(pay);
+console.log(pay);*/
+
+
+// let firstName = 'Asabeneh'
+// let lName = 'Yetayeh'
+// firstName.length > lName.length
+// console.log('Your first name, Asabeneh is longer than your family name, Yetayeh');

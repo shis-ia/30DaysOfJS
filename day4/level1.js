df030a3d844291d239053af12869d035a0fd79de
shis-ia <charityshisia7139@gmail.com>
2025-01-09 14:04:35 +0300

    level1

---
+++ day4/level1.js
@@ -0,0 +1,48 @@
+
+/*let age = prompt('enter age')
+let remainder =(18 - age)
+if (age>=18){
+    console.log(`you are old enough drive`)
+} else{
+    console.log(`wait for the ${remainder} years to turn 18.`)
+}*/
+
+/*let myage = prompt('my age')
+let yourage =prompt('your age')
+if (myage > yourage){
+    console.log('My age is greater than your age')
+} else{
+    console.log('Your age is greater than mine')
+}*/
+
+let num = 20
+let rem = (num% 2)
+if (rem > 0)
+    {console.log(`${num} is odd`)}
+else{
+    console.log(`${num} is even`)
+}
+
+//let num = 15
+//let rem =(num%2)
+//console.log(rem)
+
+
+
+
+
+
+/*let a = 2
+let b = 3
+if (a > b) 
+    { console.log(`${a} is grater than ${b}`) }
+else {
+    console.log(`${a} is less than ${b}`)
+}*/
+
+/*let a = 6
+let b = 3
+a>b
+?console.log(`${a} is greater than ${b}`)
+:console.log(`${a} is less than ${b}`)*/
+

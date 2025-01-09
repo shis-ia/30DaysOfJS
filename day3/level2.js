a846e53305e225c6ff717b43c071c822b0e245a8
shis-ia <charityshisia7139@gmail.com>
2025-01-09 10:39:54 +0300

    ternary operator

--- day3/level2.js
+++ day3/level2.js
@@ -7,7 +7,7 @@ console.log(area); //area of triangel*/
 /*let sideA = prompt('enter sideA');
 let sideB = prompt('enter sideB');
 let sideC = prompt('enter sideC');
-let perimeter = ( sideA + sideB +sideC);
+let perimeter = ( parseInt(sideA) + parseInt(sideB) + parseInt(sideC));
 console.log(perimeter);*/
 
 /*let length =prompt('enter length');
@@ -16,14 +16,12 @@ let rectarea = ((length * width));
 console.log(rectarea);*/
 
 
-let length =prompt('enter length');
+/*let length =prompt('enter length');
 let width =prompt('width');
 
 let area = 2 * ( parseInt(length) + parseInt(width) )
 
-console.log(area)
-
-// console.log(2 * (length + width));
+console.log(area)*/
 
 /*let radius =prompt('enter radius');
 const PI = (3.14);
@@ -40,7 +38,43 @@ let pay=(hours*rate);
 console.log(pay);*/
 
 
-// let firstName = 'Asabeneh'
-// let lName = 'Yetayeh'
-// firstName.length > lName.length
-// console.log('Your first name, Asabeneh is longer than your family name, Yetayeh');
+/*let firstName = 'Yetayeh'
+let lName = 'Asabeneh'
+firstName.length > lName.length
+?console.log('Your first name is longer than your lname')
+:console.log('Your lname is longer than your fastname')*/
+
+
+/*let yourname ='Joe'
+yourname.length > 7
+?console.log('yourname is long')
+:console.log('your name is short')
+yourname ='MaryJane'
+
+yourname.length > 7
+?console.log('your name is long')
+:console.log('your name is short')*/
+
+/*let year = prompt('enter year')
+let age = (2025 - year)
+let remyears =(18 - age)
+
+age>18
+?console.log(`you are ${age} years old, you can drive`)
+:console.log(`you are ${age} years old you will be alowed to drive after ${remyears} years`)*/
+
+
+/*const now = new Date
+const year = now.getFullYear()
+const month = now.getMonth() + 1
+const day = now.getDay()
+const hours = now.getHours()
+const minutes = now.getMinutes()
+console.log(`${year}/${month}/${day}/ ${hours}:${minutes}`)
+console.log(`${day}/${month}/${year}/ ${hours}:${minutes}`)
+console.log(`${day}-${month}-${year} ${hours}:${minutes}`)*/
+
+let myAge = 100
+let yourAge = 25
+let ageGap =(myAge - yourAge)
+console.log(`I am ${ageGap} years older than you`)
---
+++ day3/level3.js
@@ -0,0 +1,7 @@
+let now = new Date
+let year = now.getFullYear()
+let month = now.getMonth() + 1
+let day = now.getDay()
+let hours = now.getHours()
+let minutes = now.getMinutes()
+console.log(`${year}-0${month}-0${day} 0${hours}:${minutes}`)

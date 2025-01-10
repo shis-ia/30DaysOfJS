db932b6b492235faf0aea945059cdacea5cf340b
shis-ia <charityshisia7139@gmail.com>
2025-01-10 14:28:27 +0300

    level1

--- day4/level2.js
+++ day4/level2.js
@@ -83,3 +83,4 @@ switch (day) {
 
 
 
+
---
+++ day5/level1.js
@@ -0,0 +1,103 @@
+/*const families = Array()
+console.log(families) //empty array*/
+
+/*const numbers = [1, 2, 3, 4, 5, 6, 7]
+/*console.log(numbers) //array of more than 5 elements
+console.log('lenghth:',numbers.length) //length of array
+let thefirstnumber =numbers[0]
+console.log(thefirstnumber)
+let themiddlenumber = numbers[3]
+console.log(themiddlenumber)
+let lastindex = numbers.length - 1
+lastnumber = numbers[lastindex]
+console.log(lastnumber)
+
+//or
+
+let lastitem = numbers[6]
+console.log(lastitem)
+
+const mixedDataType= ['Mary','Doe',20,true,'Kenya',2025, false]
+console.log(mixedDataType) //array with mixed data type
+console.log(mixedDataType.length)*/
+
+const itCompamies =[ 'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' ,'Amazon']
+//console.log(itCompamies)
+/*console.log('Number of companies:',itCompamies.length)
+
+let firstcompany = itCompamies[0]
+console.log(firstcompany)
+
+let middlecompany = itCompamies[3]
+console.log(middlecompany)
+
+let lastindex = itCompamies.length - 1
+lastcompany = itCompamies[lastindex]
+console.log(lastcompany)
+
+console.log(itCompamies[0])
+console.log(itCompamies[1])
+console.log(itCompamies[2])
+console.log(itCompamies[3])
+console.log(itCompamies[4])
+console.log(itCompamies[5])
+console.log(itCompamies[6])*/
+
+/*let company1 = itCompamies[0]
+console.log(company1.toUpperCase(0))
+
+let company2 = itCompamies[1]
+console.log(company2.toUpperCase(1))
+
+let company3 = itCompamies[2]
+console.log(company3.toUpperCase(2))
+
+let company4 = itCompamies[3]
+console.log(company4.toUpperCase(3))
+
+let company5 = itCompamies[4]
+console.log(company5.toUpperCase(4))
+
+let company6 = itCompamies[5]
+console.log(company6.toUpperCase(5))
+
+let company7 = itCompamies[6]
+console.log(company7.toUpperCase(6))*/ 
+
+
+//console.log(itCompamies.toString())
+
+/*let answer = itCompamies.indexOf('Facebook')
+
+if (answer=== -1){
+    console.log(`company not found`)
+} else{
+    console.log('company')
+}*/
+
+
+/*console.log(itCompamies.sort()) //sort arrange in alphabetical order
+
+let sortarray = itCompamies.sort()
+console.log(sortarray.reverse())//sort reverse*/
+
+/*console.log(itCompamies.slice(3))
+
+console.log(itCompamies.slice(0,4))*/
+
+console.log(itCompamies.slice(1))
+console.log(itCompamies.slice(0,6))
+console.log(itCompamies.slice(7))
+
+
+
+
+
+
+
+
+
+
+
+
+

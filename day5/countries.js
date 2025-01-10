317fd1fde930e4f50ed7df5a96ad5934f2e5954e
shis-ia <charityshisia7139@gmail.com>
2025-01-10 23:04:29 +0300

    level2

---
+++ day5/countries.js
@@ -0,0 +1,39 @@
+
+
+
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
+
+
+/*let country = prompt('enter country')
+//console.log(countries)
+let answer = countries.includes(country)
+//let addCountry = countries.push(country)
+//console.log(addCountry)
+ if (answer = true){
+    console.log(`${country}`)
+ } else {
+    console.log('no')
+ }*/
+
+/*const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+let fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)*/
+
+
+
+
+
+
---
+++ day5/level2.js
@@ -0,0 +1,19 @@
+/*const text = 
+"I love teaching and empowering people I teach HTML CSS JS React Python."
+const words = text.split(' ')
+
+console.log(words)
+console.log(words.length)*/
+
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+shoppingCart.unshift('Meat')
+console.log(shoppingCart)
+shoppingCart.push('Sugar')
+console.log(shoppingCart)
+shoppingCart.pop('Honey')
+console.log(shoppingCart)
+shoppingCart[3] = 'Green Tea'
+console.log(shoppingCart)
+
+

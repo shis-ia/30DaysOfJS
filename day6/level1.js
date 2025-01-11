727d209ca7fb0b355876c7c1af8002c8490ac4d8
shis-ia <charityshisia7139@gmail.com>
2025-01-11 14:56:35 +0300

    level1

---
+++ day6/level1.js
@@ -0,0 +1,81 @@
+/*for (let i =0; i <= 10; i ++)
+    console.log(i) //for loop
+
+//while loop
+let i = 0
+while(i <= 5){
+    console.log(i)
+    i ++
+}
+
+//do while loop
+let i =0
+do {
+    console.log(i)
+    i ++
+} while (i <=10)
+
+for (let i = 10; i >= 0; i--)
+    console.log(i)
+
+let i = 10
+while (i >= 1) {
+    console.log(i)
+    i --
+}
+
+let i =10
+do {
+    console.log(i)
+    i --
+} while (i >= 1)
+
+const numbers = [1, 2, 3, 4, 5,6,7,8,9,10]
+let sum = 0
+for (let i = 0; i < numbers.length; i++) {
+    console.log(`${i} * ${i} = ${i*i}`)
+}
+
+
+//even numbers from 1-100
+for (var i = 0; i <= 100; i++){
+if (i % 2 == 0)
+    console.log(i)
+}
+ 
+//odd numbers from 1 -100
+for (var i =0; i <= 100; i++)
+    if ((i % 2) == 1)
+        console.log(i)
+
+
+    //sum of numbers from 1-100
+sum = 0
+for (var i = 0; i <= 100; i++){
+    sum = sum + i
+}
+console.log(sum)
+
+//sum of even numbers from 1 - 100
+sum =0
+for (var i = 0; i <= 100; i++)
+    if (i % 2 == 0)
+        sum = sum + i
+        console.log(sum)
+
+//sum of odd numbers from 1 -100
+ sum = 0
+for (var i = 0;i <= 100; i++)
+    if (i % 2 != 0)
+        sum = sum + i
+        console.log(sum)*/
+
+
+        
+        
+
+
+
+
+
+

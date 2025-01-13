b5cc4a4fb7da15b513632d9abd1b60085faf4dcc
shis-ia <charityshisia7139@gmail.com>
2025-01-13 22:06:01 +0300

    loop arry of numbers

--- day6/level1.js
+++ day6/level1.js
@@ -1,4 +1,6 @@
-/*for (let i =0; i <= 10; i ++)
+
+ /*       Q1
+for (let i =0; i <= 10; i ++)
     console.log(i) //for loop
 
 //while loop
@@ -14,7 +16,7 @@ do {
     console.log(i)
     i ++
 } while (i <=10)
-
+       Q2
 for (let i = 10; i >= 0; i--)
     console.log(i)
 
@@ -29,20 +31,22 @@ do {
     console.log(i)
     i --
 } while (i >= 1)
-
+         Q3
+for (let i = 0; i <= n; i++)
+         Q5
 const numbers = [1, 2, 3, 4, 5,6,7,8,9,10]
 let sum = 0
 for (let i = 0; i < numbers.length; i++) {
     console.log(`${i} * ${i} = ${i*i}`)
 }
 
-
+        Q7
 //even numbers from 1-100
 for (var i = 0; i <= 100; i++){
 if (i % 2 == 0)
     console.log(i)
 }
- 
+        Q8
 //odd numbers from 1 -100
 for (var i =0; i <= 100; i++)
     if ((i % 2) == 1)
@@ -54,44 +58,71 @@ sum = 0
 for (var i = 0; i <= 100; i++){
     sum = sum + i
 }
-console.log(sum)
-
+console.log(sum)*?
+            Q10/11
 //sum of even numbers from 1 - 100
-sum =0
+let resultsarray =[]
+sumeven =0
 for (var i = 0; i <= 100; i++)
     if (i % 2 == 0)
-        sum = sum + i
-        console.log(sum)*/
-
+        sumeven = sumeven + i
+        console.log(sumeven)
+          Q12
 //sum of odd numbers from 1 -100
-/*arry = []
-arryOdd=[]
-sum = 0
+sumodd = 0
+for (var i = 0; i <= 100; i++)
+    if (i % 2 != 0) 
+        sumodd = sumodd + i
+    console.log(sumodd)
+let results = [];
+results[0] = sumeven;
+results[1] = sumodd
+console.log(results)*/
 
-for (var i = 0;i <= 100; i++){
-   if (i % 2 == 0)
-   sum = sum + i
-arry.push(sum)
-   console.log(arry)}
-   else {
-       if (i % 2 != 0)
-           sum = sum +1
-       arryOdd.push(sum)
-       console.log(arryOdd)
-   }
-   //if (i % 2 != 0)
-      //sum = sum + i
-      //console.log(sum)*/
 
-/*let ranNum = Math.floor(Math.random()*6)
-       num=[]
-       //console.log(num)
-       for (var i = 1; i <= ranNum;i++)
-        num.push(i)
-        console.log(num)*/
+/*for (var i = 0; i <= 100; i++)
+    if (i % 2 != 0)
+        sum = sum + i
+console.log(sum)*/
 
-    //LEVEL 2 NO 4
-const countries = [
+
+//if (i % 2 != 0)
+//sum = sum + i
+//console.log(sum)
+          //Q13
+/*let numbers = []
+
+   for (var i = 0; i < 5; i++){
+    numbers[i] = parseInt(Math.random()*9)
+ } console.log(numbers)
+
+
+            Q4
+/*   //pattern of #
+var row = 7;
+//var cols = 5;
+for (let i = 1; i <= 7; i++) {
+ let output_string = '#';
+ for (let j = 1; j <= (i - 1); j++) {
+         output_string = output_string +'#';
+     }
+     console.log(output_string)
+ }*/
+
+let array = []
+for (let i = 1; i <= 7; i++) {
+    random = parseInt(Math.random(i) * 9)
+
+} console.log(random)
+
+
+
+
+
+
+
+//LEVEL 2 NO 4
+/*const countries = [
     'Albania',
     'Bolivia',
     'Canada',
@@ -107,7 +138,7 @@ const countries = [
 
 for (let i = 0; i<= countries.lenght;i++)
     countries(i)
-    console.log(countries)
+    console.log(countries)*/
 
 
 

68769359f08cb8c2292a253f0e7d8b634b6f24e3
shis-ia <charityshisia7139@gmail.com>
2025-01-24 11:37:58 +0300

    level2

---
+++ day10A/level2.js
@@ -0,0 +1,40 @@
+
+// //Q1
+// const a = [4, 5, 8, 9]
+// const b = [3, 4, 5, 7]
+// const c = [...a,...b]
+
+// const B = new Set(b)
+// const A = new Set(a)
+// const C = new Set(c)
+
+// console.log(c)
+
+
+
+// //Q2
+// const a = [4, 5, 8, 9]
+// const b = [3, 4, 5, 7]
+
+// const B = new Set(b)
+// const A = new Set(a)
+
+// let c = a.filter((num) => B.has(num))
+// let C = new Set(c)
+
+// console.log(C)
+
+
+
+
+//  * //Q3
+// const a = [4, 5, 8, 9]
+// const b = [3, 4, 5, 7]
+
+// const B = new Set(b)
+// const A = new Set(a)
+
+// let c = a.filter((num) => !B.has(num))
+// let C = new Set(c)
+
+// console.log(C)

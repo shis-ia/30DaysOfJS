85565536d031109e1ec99e284c4a4cb60ca20050
shis-ia <charityshisia7139@gmail.com>
2025-01-24 11:08:58 +0300

    leve1 creating set and maps

---
+++ day10A/level1.js
@@ -0,0 +1,43 @@
+
+// //Q1
+// const names = new Set ()
+// console.log(names)
+
+// //Q2
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// setOfNumbers = new Set()
+// for(const num of numbers){
+//     setOfNumbers.add(num)
+// }
+// console.log(setOfNumbers)
+
+
+// //Q3
+// const names = ['John','James','Andrew','Marther','Mary','Elizabeth']
+// setOfNames = new Set(names)
+// console.log(setOfNames.delete('Mary'))
+// console.log(setOfNames.size)
+
+// // Q4
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// setOfNumbers = new Set(numbers)
+// console.log(setOfNumbers.size)
+// console.log(setOfNumbers.clear())
+// console.log(setOfNumbers.size)
+
+// //Q5
+// const subjects = ['English','Science','Kiswahili','CRE','SocialStudies']
+// setOfSubjescts = new Set(subjects)
+// console.log(setOfSubjescts)
+
+//Q6
+const countries = new Map()
+mapOfCountries = new Map(countries)
+mapOfCountries.set('Kenya',5)
+mapOfCountries.set('Uganda',6)
+mapOfCountries.set('Cuber',5)
+mapOfCountries.set('SouthAfrica',11)
+mapOfCountries.set('Tanzania',8)
+
+console.log(mapOfCountries)

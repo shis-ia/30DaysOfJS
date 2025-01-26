3012bd3da407484a6585e29f8c8601bd8b9e5d3f
shis-ia <charityshisia7139@gmail.com>
2025-01-26 21:34:52 +0300

    level2 warn,assert and error

---
+++ day13/level2.js
@@ -0,0 +1,13 @@
+//Q1
+
+console.assert(10 > 2*10, '10 is not greater than 2*10')
+
+
+//Q2
+
+console.warn('you are not 18 years old')
+
+
+//Q3
+
+console.error('you have entered the wrong passcode')

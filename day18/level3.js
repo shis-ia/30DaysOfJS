bb2901db1c210f92cf7c0278b037ad5b01fb253f
shis-ia <charityshisia7139@gmail.com>
2025-01-30 22:55:57 +0300

    fetching data from API

--- day18/level3.js
+++ day18/level3.js
@@ -1,17 +1,19 @@
-
 const catsAPI = 'https://api.thecatapi.com/v1/breeds'
 const fetchData = async () => {
     try {
         const response = await fetch(catsAPI)
         const cats = await response.json()
         for (cat in cats) {
-            let name = cats[cat].name
-             console.log(`name:${name}`)
+            let weight = cats[cat].weight
+            // let avarageWeight =weight
+            // console.log(avarageWeight)
+            console.log(weight)
         }
-        
+        // console.log(cats)
     } catch (err) {
         console.error(err)
     }
 }
  fetchData()
+
 
